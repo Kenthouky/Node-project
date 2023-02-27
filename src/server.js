f0bf2d7a1cd953b6794e17e77a2e4af089b21289
Dollars Kenthouky <dollarskenthouky@gmail.com>
2023-02-27 09:37:20 +0100

    two

--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@
  * This is the starting point for the app.
  */
 
-const http = require('http');
-const app = require('./index');
+import { createServer } from 'http';
+import app from './index';
 
 const port = process.env.PORT || 5000;
-const server = http.createServer(app);
+const server = createServer(app);
 
 server.listen(port, hostname, function () {
 		console.log(`Started on port ${port}`);
